18375dc55a765bda7808e21ecbebe78bcd80697f
Hussein380 <huznigarane@gmail.com>
2024-01-13 01:10:01 -0800

    corrected betty for this file

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -15,7 +15,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	/*ccheck if the memory allocation for the new_node was successsfull*/
 	if (new_node == NULL)
-		return NULL;
+		return (NULL);
 	/*initialize new_node with provided value*/
 	new_node->n = n;
 	new_node->next = NULL;
@@ -23,16 +23,14 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	/*check for case where idx is zero, at that point we add it at the beginning*/
 	if (idx == 0)
-	{
 		return (add_dnodeint(h, n));
-	}
 	/*traverse the list to the specified index or the end of the list*/
 	while (current != NULL && i < idx - 1)
 	{
 		current = current->next;
 		i++;
 	}
-	/*if the index is beyond the end of list , free allocated memory and return NULL*/
+	/*if the index is beyond the end of list , free allocated memoryL*/
 	if (current == NULL)
 	{
 		free(new_node);
@@ -40,9 +38,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 	/*if the index is at the end or beyond insert it at the end*/
 	if (current->next == NULL)
-	{
 		return ((add_dnodeint_end(h, n)));
-	}
 	/*Insert the new node into the list at specified index:*/
 	new_node->prev = current;
 	new_node->next = current->next;
